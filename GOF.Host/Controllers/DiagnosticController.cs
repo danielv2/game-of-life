723aa4dce5aea44c3d9d6f8ddda8928823e89adb
Daniel Vieira <daniel-v2@hotmail.com>
2024-10-18 04:12:32 -0300

    Controller to validate path volume

---
+++ GOF.Host/Controllers/DiagnosticController.cs
@@ -0,0 +1,46 @@
+
+using System.IO;
+using GOF.Domain.Configurations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace GOF.Host.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DiagnosticsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<DiagnosticsController> _logger;
+
+        public DiagnosticsController(IConfiguration configuration, ILogger<DiagnosticsController> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        [HttpGet("check-db")]
+        public IActionResult CheckDatabaseFile()
+        {
+            // Obtém a connection string do appsettings.json
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            // Loga a connection string
+            _logger.LogInformation($"ConnectionString: {connectionString}");
+
+            var dbPath = "/app/data";
+            if (Directory.Exists(dbPath))
+            {
+                var files = Directory.GetFiles(dbPath);
+                return Ok(new
+                {
+                    ConnectionString = connectionString,
+                    Files = files
+                });
+            }
+
+            return NotFound(new { Message = "Directory not found.", ConnectionString = connectionString });
+        }
+    }
+}
